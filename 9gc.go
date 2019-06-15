a676fa67756a522852afd9685dccb8b4945eae0e
tesujiro <tesujiro@gmail.com>
2019-06-15 16:07:33 +0900

    finished step2

--- 9gc.go
+++ 9gc.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -10,4 +12,14 @@ func main() {
 		log.Fatalf("引数の個数が正しくありません\n")
 	}
 
+	fmt.Printf(".intel_syntax noprefix\n")
+  	fmt.Printf(".global main\n")
+  	fmt.Printf("main:\n")
+  	expr,err :=strconv.Atoi(os.Args[1])
+	if err!=nil{
+		log.Fatalf("引数が正しくありません\n")
+	}
+  	fmt.Printf("  mov rax, %d\n", expr)
+  	fmt.Printf("  ret\n")
+  	return 
 }
